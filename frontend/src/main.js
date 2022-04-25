3b8efa7acf2a7522d317bf658b9eb9f75f5b2637
Mingyu Choi <me@somni.one>
2022-04-25 22:18:11 +0900

    Use `normalize.css`

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./plugins/router";
+import App from "@/App.vue";
+import router from "@/plugins/router";
+
+import "normalize.css";
+import "@/styles/main.scss";
 
 createApp(App)
   .use(router)
