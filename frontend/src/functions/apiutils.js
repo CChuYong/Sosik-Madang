d04286451ee72e6c41c80b178d182a6e468bd028
Mingyu Choi <me@somni.one>
2022-04-28 20:55:58 +0900

    API utils return data directly

--- frontend/src/functions/apiutils.js
+++ frontend/src/functions/apiutils.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 async function apiGet(endpoint) {
-  return await axios.get(`${process.env.VUE_APP_API_HOST}${endpoint}`, {
+  return (await axios.get(`${process.env.VUE_APP_API_HOST}${endpoint}`, {
     responseType: "json",
-  });
+  })).data;
 }
 
 async function apiPost(endpoint, jsonData) {
   const data = typeof(jsonData) !== "string" ? JSON.parse(jsonData) : jsonData;
 
-  return await axios.post(`${process.env.VUE_APP_API_HOST}${endpoint}`, data, {
+  return (await axios.post(`${process.env.VUE_APP_API_HOST}${endpoint}`, data, {
     responseType: "json",
-  });
+  })).data;
 }
 
 async function apiPut(endpoint, jsonData) {
   const data = typeof(jsonData) !== "string" ? JSON.parse(jsonData) : jsonData;
 
-  return await axios.put(`${process.env.VUE_APP_API_HOST}${endpoint}`, data, {
+  return (await axios.put(`${process.env.VUE_APP_API_HOST}${endpoint}`, data, {
     responseType: "json",
-  });
+  })).data;
 }
 
 export default {
