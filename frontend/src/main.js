afcbd6819bd6c9fec6f9f3b8a9997d9eedf805f5
Mingyu Choi <me@somni.one>
2022-04-28 20:21:12 +0900

    Add vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import App from "@/App.vue";
 import router from "@/plugins/router";
+import store from "./plugins/store";
 
 import "normalize.css";
 import "@/styles/main.scss";
 
 createApp(App)
+  .use(store)
   .use(router)
   .mount("#app");
